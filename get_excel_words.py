40745ad653a11633538fb5a28baab4a410d63131
Yuko Sasaki <sasakiy.2233@gmail.com>
2020-09-19 17:19:27 +0900

    make excel words datatext

--- get_excel_words.py
+++ get_excel_words.py
@@ -1,30 +1,60 @@
-
+from collections import defaultdict
+from janome.tokenizer import Tokenizer
 import pandas as pd
+import pprint
 import re
 
 from config import EXCEL_NAME, SHEET_NAMES
 
 
-def remove_unwanted(wordx):
-    return re.sub('[\s\R※　]', '',wordx)
-
-def is_add_set(cellx):
-    if type(cell) is str:
-        return True
-    else:
-        return False
-
+def is_number(wordx):
+    return re.search('[a-zA-Z0-9-－%()【】「」　]+', wordx)
 
 df = pd.read_excel(EXCEL_NAME, sheet_name = None , header = None)
 
-words = set()
+excel_words = []
 for sheet_name in SHEET_NAMES:
     for index, row in df[sheet_name].iterrows():
         for cell in row:
-            if is_add_set(cell) :
-                words.add(cell)
-    words.add(sheet_name)
+            if type(cell) is str:
+                excel_words.append(cell)
+    excel_words.append(sheet_name)
 
-words = list(words)
-print(words)
-print(words[0])
+excel_words = list(set(excel_words))
+#print(words)
+#print(words[0])
+
+t = Tokenizer(mmap=True)
+
+# 分かち書きして特定の品詞除く方法
+wakati_words = []
+for word in excel_words:
+    for token in t.tokenize(word):
+        if "記号" in token.part_of_speech or "記号" in token.part_of_speech:#  or "助" in token.part_of_speech
+            continue
+        if is_number(token.surface):#  or len(token.surface) < 2
+            continue
+        """
+        if "名" in token.part_of_speech:
+            wakati_words.append(token.surface)"""
+        wakati_words.append(token.surface)
+wakati_words = list(set(wakati_words))
+pprint.pprint(wakati_words)
+
+
+# 数で並べ替える
+wakati_dict = defaultdict(int)
+for word in excel_words:
+    for token in t.tokenize(word):
+        if "記号" in token.part_of_speech or "記号" in token.part_of_speech:# or "助" in token.part_of_speech
+            continue
+        if is_number(token.surface):# or len(token.surface) < 2
+            continue
+        """
+        if "名" in token.part_of_speech:
+            wakati_dict[token.surface] += 1"""
+        wakati_dict[token.surface] += 1
+
+pprint.pprint(wakati_dict)
+print(len(wakati_words))
+print(len(wakati_dict))
